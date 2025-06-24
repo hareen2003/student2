3d226f15f7c397c2a1a0d157079a6ebf1ee57bb9
hareen2003 <senethmasethumi@gmail.com>
2025-06-24 23:45:31 +0530

    First Commit

---
+++ src/studentmanagementsystem2/MainApp.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentmanagementsystem2;
+
+ public class MainApp {
+    public static void main(String[] args) {
+        DatabaseConnection.getConnection(); // Call the method to test connection
+    }
+}
